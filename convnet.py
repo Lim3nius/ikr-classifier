bac09a4b6be18e944b0b1ce0c5103762abc683c2
lim3nius <lessy.mot@gmail.com>
2018-04-28 20:33:38 +0200

    Convnet - loss ~~ 0.4

--- convnet.py
+++ convnet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from ikrlib import *
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
@@ -10,29 +9,22 @@ from keras import backend as K
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 
-def png2fea(dir_name):
-    """
-    Loads all *.png images from directory dir_name into a dictionary. Keys are the file names
-    and values and 2D numpy arrays with corresponding grayscale images
-    """
-    features = {}
-    for f in glob(dir_name + '/*.png'):
-        print("Processing file: ", f)
-        features[f] = imread(f, True).astype(np.float64).flatten()
-    return features
-
 
 #image dimensions
 img_height, img_width=80, 80
 
-# dir = ['data/target_train', 'data/non_target_train', 'data/target_dev', 'data/non_target_dev']
 
 # Possible to add some transformations for bigger variability - smaller chance of over training and specializations
-train_data_generator = ImageDataGenerator(horizontal_flip=True, rotation_range=20) # Some rotation & flipping
+train_data_generator = ImageDataGenerator(horizontal_flip=True,
+                                          rotation_range=20) # Some rotation & flipping
 test_data_generator = ImageDataGenerator()
 
-train_gen = train_data_generator.flow_from_directory('data/train', target_size=(img_width,img_height))
-test_gen = test_data_generator.flow_from_directory('data/validation', target_size=(img_width,img_height))
+train_gen = train_data_generator.flow_from_directory('data/train',
+                                                     target_size=(img_width,img_height),
+                                                     batch_size=8)
+test_gen = test_data_generator.flow_from_directory('data/validation',
+                                                   target_size=(img_width,img_height),
+                                                   batch_size=8)
 
 
 # Set correct input shape
@@ -42,31 +34,58 @@ else:
     input_shape=(img_width,img_height,3)
 
 epochs = 10
-batch_size = 10
+batch_size = 8
 number_of_classes = 2
 
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
-#model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(32, kernel_size=(5, 5), activation='softmax', input_shape=input_shape))
+# model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(Activation('softmax'))
+model.add(MaxPooling2D(pool_size=(2, 2))) # Make average
+# model.add(Dropout(0.25))
+# model.add(Flatten())
+
+# change parameters
+model.add(Conv2D(64, kernel_size=(5,5)))
+model.add(Activation('softmax'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+# model.add(Dropout(0.25))
+model.add(Dropout(0.5))
+# model.add(Dense(256, activation='relu'))
+
+model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.3))
+
+model.add(Flatten()) # convert 3D to 1D features
+model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.25)) # overtraining protection
 model.add(Dense(number_of_classes, activation='softmax')) # The last layer - determining who is who
 
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
+# model.compile(loss=keras.losses.categorical_crossentropy,
+#               # optimizer=keras.optimizers.Adadelta(),
+#               optimizer=keras.optimizers.SGD,
+#               metrics=['accuracy'])
+
+model.compile(loss='categorical_crossentropy', # categorical because of 2 classes
+              optimizer='sgd',
               metrics=['accuracy'])
 
 
 model.fit_generator(train_gen,
-                    steps_per_epoch=128,
+                    steps_per_epoch=train_gen.samples // batch_size,
                     epochs=epochs,
                     validation_data=test_gen,
-                    validation_steps=60)
-
-
+                    validation_steps=train_gen.samples // batch_size)
 
 score = model.evaluate_generator(test_gen)
 model.summary()
+
+# model.predict_generator(test_gen)
+results = list(zip(test_gen.filenames, model.predict_generator(test_gen)))
+results = [(x,list(y)) for (x,y) in results]
+
+print(results)
+
